cebadcdcf0e97c8ec0f713272050a6646b620d55
Rubèn <numbworks@gmail.com>
2026-01-23 20:28:23 +0000

    #1 CLIManagerTestCase: added (49%).

--- tests/nwversioninfofilestests.py
+++ tests/nwversioninfofilestests.py
@@ -3,13 +3,13 @@ import unittest
 from argparse import ArgumentParser, Namespace
 from datetime import datetime, timezone
 from parameterized import parameterized
-from typing import Callable, Literal, Optional, Tuple
+from typing import Any, Callable, Literal, Optional, Tuple
 from unittest.mock import Mock, mock_open, patch
 
 # LOCAL MODULES
 import sys, os
 sys.path.append(os.path.dirname(__file__).replace('tests', 'src'))
-from nwversioninfofiles import VersionInfoFileCreator, VersionInfoFileWriter, VersionInfoFileVerifier
+from nwversioninfofiles import VersionInfoFileCreator, VersionInfoFileWriter, VersionInfoFileVerifier, CLIManager, CLISTRING
 
 # SUPPORT METHODS
 # TEST CLASSES
@@ -138,9 +138,6 @@ class VersionInfoFileWriterTestCase(unittest.TestCase):
              patch("builtins.open", mock_open()) as mocked_open:
             
             vinf_writer : VersionInfoFileWriter = VersionInfoFileWriter()
-            print("test_write_shouldreturntrue")
-            print(len(vinf_writer.messages))
-
             actual : bool = vinf_writer.write(content = content, output_path = output_path)
 
             # Assert
@@ -154,16 +151,14 @@ class VersionInfoFileWriterTestCase(unittest.TestCase):
         # Arrange
         content : str = "Test content"
         output_path : str = "test_folder/version_info_file.txt"
-        mocked_exception : OSError = OSError("Permission denied")
+        exception : OSError = OSError("Permission denied")
         expected_status : bool = False
         expected_message : str = "The provided Version Info File ('test_folder/version_info_file.txt') has not been written to disk ('Permission denied')."
 
         # Act
-        with patch("os.makedirs", side_effect = mocked_exception):
+        with patch("os.makedirs", side_effect = exception):
             
             vinf_writer : VersionInfoFileWriter = VersionInfoFileWriter()
-            print("test_write_shouldreturnfalse")
-            print(len(vinf_writer.messages))
             actual : bool = vinf_writer.write(content = content, output_path = output_path)
 
             # Assert
@@ -213,8 +208,8 @@ class VersionInfoFileVerifierTestCase(unittest.TestCase):
         # Arrange
         file_path : str = "test_folder/version_info_file.txt"      
         vinf_verifier : VersionInfoFileVerifier = VersionInfoFileVerifier()
-        mocked_exception : Exception = Exception("Invalid format")
-        mocked_lvi : Mock = Mock(side_effect = mocked_exception)
+        exception : Exception = Exception("Invalid format")
+        mocked_lvi : Mock = Mock(side_effect = exception)
 
         expected_status : bool = False
         expected_message : str = "The provided Version Info File ('test_folder/version_info_file.txt') is not compliant with PyInstaller ('Invalid format')."
@@ -245,6 +240,85 @@ class VersionInfoFileVerifierTestCase(unittest.TestCase):
         # Assert
         self.assertEqual(expected_status, actual)
         self.assertEqual(vinf_verifier.messages[0], expected_message)
+class CLIManagerTestCase(unittest.TestCase):
+
+    def test_initializeparser_shouldaddexpectedarguments_wheninvoked(self) -> None:
+
+        # Arrange
+        argument_parser : Mock = Mock(spec = ArgumentParser)
+
+        # Act
+        cli_manager : CLIManager = CLIManager(argument_parser = argument_parser)
+        cli_manager._CLIManager__initialize_parser()  # type: ignore
+        calls : Any = argument_parser.add_argument.call_args_list
+
+        # Assert
+        self.assertEqual(argument_parser.add_argument.call_count, 8)
+        
+        self.assertEqual(calls[0][0][0], CLISTRING.OPTION_COMPANYNAME_FLAGS[0])
+        self.assertEqual(calls[0][1]['required'], CLISTRING.OPTION_COMPANYNAME_REQUIRED)
+        self.assertEqual(calls[0][1]['help'], CLISTRING.OPTION_COMPANYNAME_HELP)
+        
+        self.assertEqual(calls[1][0][0], CLISTRING.OPTION_FILEDESCRIPTION_FLAGS[0])
+        self.assertEqual(calls[1][1]['required'], CLISTRING.OPTION_FILEDESCRIPTION_REQUIRED)
+        self.assertEqual(calls[1][1]['help'], CLISTRING.OPTION_FILEDESCRIPTION_HELP)
+        
+        self.assertEqual(calls[2][0][0], CLISTRING.OPTION_FILEVERSION_FLAGS[0])
+        self.assertEqual(calls[2][1]['required'], CLISTRING.OPTION_FILEVERSION_REQUIRED)
+        self.assertEqual(calls[2][1]['help'], CLISTRING.OPTION_FILEVERSION_HELP)
+        
+        self.assertEqual(calls[3][0][0], CLISTRING.OPTION_LEGALCOPYRIGHT_FLAGS[0])
+        self.assertEqual(calls[3][1]['required'], CLISTRING.OPTION_LEGALCOPYRIGHT_REQUIRED)
+        self.assertEqual(calls[3][1]['help'], CLISTRING.OPTION_LEGALCOPYRIGHT_HELP)
+        
+        self.assertEqual(calls[4][0][0], CLISTRING.OPTION_ORIGINALFILENAME_FLAGS[0])
+        self.assertEqual(calls[4][1]['required'], CLISTRING.OPTION_ORIGINALFILENAME_REQUIRED)
+        self.assertEqual(calls[4][1]['help'], CLISTRING.OPTION_ORIGINALFILENAME_HELP)
+        
+        self.assertEqual(calls[5][0][0], CLISTRING.OPTION_PRODUCTNAME_FLAGS[0])
+        self.assertEqual(calls[5][1]['required'], CLISTRING.OPTION_PRODUCTNAME_REQUIRED)
+        self.assertEqual(calls[5][1]['help'], CLISTRING.OPTION_PRODUCTNAME_HELP)
+        
+        self.assertEqual(calls[6][0][0], CLISTRING.OPTION_OUTPUTPATH_FLAGS[0])
+        self.assertEqual(calls[6][1]['required'], CLISTRING.OPTION_OUTPUTPATH_REQUIRED)
+        self.assertEqual(calls[6][1]['help'], CLISTRING.OPTION_OUTPUTPATH_HELP)
+        
+        self.assertEqual(calls[7][0][0], CLISTRING.OPTION_VERIFY_FLAGS[0])
+        self.assertEqual(calls[7][1]['required'], CLISTRING.OPTION_VERIFY_REQUIRED)
+        self.assertEqual(calls[7][1]['help'], CLISTRING.OPTION_VERIFY_HELP)
+        self.assertEqual(calls[7][1]['action'], CLISTRING.OPTION_VERIFY_ACTION)
+    def test_getdefaultoutputpath_shouldreturnexpectedstring_wheninvoked(self) -> None:
+
+        # Arrange
+        cli_manager : CLIManager = CLIManager()
+        original_filename : str = "myapp.exe"
+        expected : str = "/current/directory/myapp.txt"
+        
+        with patch("os.getcwd", return_value = "/current/directory"), \
+             patch("os.path.splitext", return_value = ("myapp", ".exe")):
+            
+            # Act
+            actual : str = cli_manager._CLIManager__get_default_output_path(original_filename = original_filename)  # type: ignore
+
+            # Assert
+            self.assertEqual(expected, actual)
+    def test_printmessages_shouldprintmessagetuple_wheninvoked(self) -> None:
+
+        # Arrange
+        print_function : Mock = Mock()      
+        messages : tuple[str, ...] = tuple(["Message 1", "Message 2", "Message 3"])
+
+        # Act
+        cli_manager : CLIManager = CLIManager(print_function = print_function)        
+        cli_manager._CLIManager__print_messages(messages = messages)  # type: ignore
+
+        # Assert
+        self.assertEqual(print_function.call_count, 3)
+        print_function.assert_any_call(messages[0])
+        print_function.assert_any_call(messages[1])
+        print_function.assert_any_call(messages[2])
+
+
 
 # MAIN
 if __name__ == "__main__":
