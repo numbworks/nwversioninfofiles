43db624eeee3bf8e5edc94c912ffcca2d43cc5f0
numbworks <numbworks@gmail.com>
2026-05-11 15:35:28 +0200

    #2 setupinfo.py: added.

---
+++ src/setupinfo.py
@@ -0,0 +1,14 @@
+'''Contains project information.'''
+
+# INFORMATION
+PROJECT_VERSION : str = "2.0.0"
+PROJECT_AUTHOR : str = "numbworks"
+PROJECT_ALIAS : str = "nwvinf"
+
+LIBRARY_NAME : str = "nwversioninfofiles"
+LIBRARY_DESCRIPTION : str = "A library that facilitates the creation of Version Info Files for PyInstaller."
+
+CLI_NAME : str = "nwversioninfofilescli"
+CLI_DESCRIPTION : str = "A CLI application that facilitates the creation of Version Info Files for PyInstaller."
+
+PROJECT_URL : str = f"https://github.com/{PROJECT_AUTHOR}/{LIBRARY_NAME}"
